798b040252aeeebc2f486582d06b60057b5cd961
mermer-mac <mincoln419@naver.com>
2024-03-09 13:10:07 +0900

    todo back end init

---
+++ todo-api/src/main/java/org/mermer/todoapi/controller/TodoController.java
@@ -0,0 +1,10 @@
+package org.mermer.todoapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+
+
+}
---
+++ todo-api/src/main/java/org/mermer/todoapi/entity/TodoItem.java
@@ -0,0 +1,17 @@
+package org.mermer.todoapi.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TodoItem {
+
+	private Long id;
+
+	private String name;
+
+	private LocalDate targetDate;
+
+	private LocalDateTime createdAt;
+
+
+}
