0fdfb5f28fc5e2cf259e40cfedc6b886e8edb657
mermer-mac <mincoln419@naver.com>
2025-05-17 13:01:58 +0900

    user signin history, password encrypt

---
+++ todo-api/src/test/java/org/mermer/todoapi/entity/TimeUserTest.java
@@ -0,0 +1,24 @@
+package org.mermer.todoapi.entity;
+
+import org.junit.jupiter.api.Test;
+import org.mermer.todoapi.util.PasswordUtil;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TimeUserTest {
+
+
+	@Test
+	void setPassword() {
+		String password = "1234";
+		TimeUser user =
+
+
+	}
+
+	@Test
+	void verifyPassword() {
+	}
+}
